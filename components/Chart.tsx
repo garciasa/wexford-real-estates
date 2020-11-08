7a42e6cef25f4b3866e55a8f62c13490138fcce9
Antonio G <garciasa@gmail.com>
2020-11-08 18:30:17 +0000

    chart responsive

--- components/Chart.tsx
+++ components/Chart.tsx
@@ -1,9 +1,7 @@
 import { Flex, Text } from '@chakra-ui/core';
 import React, { ReactElement } from 'react';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import {StatChart} from '../model/interfaces'
-const data = [{name: 'Ene', uv: 5, pv: 2400, amt: 2400},
-              {name: 'Feb', uv: 4, pv: 2400, amt: 2400}];
 interface Props {
   data: StatChart[]
 }
@@ -24,14 +22,16 @@ function Chart({data}:Props): ReactElement {
       <Text mb={2} fontWeight="bold">
         {'Published Houses 4 beds (myhome.ie)'}
       </Text>
-      <LineChart width={600} height={300} data={filtered_data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-        <Line type="monotone" dataKey="houses" stroke="#8884d8" />
-        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-        <Tooltip />
-        <Legend />
-        <XAxis dataKey="month_str" />
-        <YAxis />
-      </LineChart> 
+      <ResponsiveContainer>
+        <LineChart  data={filtered_data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+          <Line type="monotone" dataKey="houses" stroke="#8884d8" />
+          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+          <Tooltip />
+          <Legend />
+          <XAxis dataKey="month_str" />
+          <YAxis />
+        </LineChart>
+      </ResponsiveContainer>
     </Flex>
   );
 }

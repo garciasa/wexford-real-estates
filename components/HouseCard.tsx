18b1096175d9aca39a8d0cc6de8d3abde5730a4b
garcia <a.garcia@epa.ie>
2022-02-16 15:46:07 +0000

    fixing charts for getting years automatically

--- components/HouseCard.tsx
+++ components/HouseCard.tsx
@@ -10,7 +10,7 @@ interface Props {
   baths: number;
   beds: number;
   provider: string;
-  date_renewed: string
+  date_renewed: string;
 }
 
 function HouseCard({ title, image, url, price, baths, beds, provider, date_renewed }: Props): ReactElement {
@@ -18,15 +18,15 @@ function HouseCard({ title, image, url, price, baths, beds, provider, date_renew
   return (
     <Box borderWidth="1px" borderRadius={8} p={1} mb={2} backgroundColor={colorMode === 'light' ? 'white' : 'gray.800'}>
       <Flex>
-        <AspectRatio w="100%" maxWidth="120px" ratio={1}>          
-            <Image src={image} alt={`Image of ${title}`} unsized />          
+        <AspectRatio w="100%" maxWidth="120px" ratio={1}>
+          <Image src={image} alt={`Image of ${title}`} layout="fill" />
         </AspectRatio>
         <Stack ml={3} mt={2} mb={2}>
           <Flex align="baseline">
             <Badge fontSize="1em">{price}</Badge>
-              <Badge ml={2} variant="subtle" colorScheme="purple">
-                {format(new Date(date_renewed), "dd/MM/yy")}          
-              </Badge>
+            <Badge ml={2} variant="subtle" colorScheme="purple">
+              {format(new Date(date_renewed), 'dd/MM/yy')}
+            </Badge>
           </Flex>
           <Box fontSize="xl" fontWeight="semibold" lineHeight="short">
             <a href={url} target="_blank" rel="noreferrer">
